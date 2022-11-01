f13a42bf4f70c77a9580c9d718e95b5bede61a82
shiva <kamarapushivachandra@gmail.com>
2022-11-02 01:11:25 +0530

    adding additinonal key to json logger in runtime

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"shiva/logger"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", ping)
-	logger.Info("application started")
+	logger.Info("application started", zap.String("additional_key_runtime", "value"))
 	router.Run(":8080")
 
 }
